b8d9c160eab5c16a07501f05d935194120f64fa9
Rice guy <89035491+cooker-of-rice@users.noreply.github.com>
2024-11-17 19:45:29 +0100

    operation mocking bird

--- session/a.php
+++ session/a.php
@@ -1,22 +1,44 @@
 <?php
-$randomValue = rand(1, 100);
+// Generování náhodné hodnoty
+$randomNumber = rand(1, 100);
 
-echo "<h1>Generovaná hodnota: $randomValue</h1>";
-echo "<p>Odkazy na b.php s různými metodami:</p>";
+// Spuštění session pro použití session proměnných
+session_start();
+$_SESSION['randomNumber'] = $randomNumber;
 
-echo "<a href='b.php?value=$randomValue'>Předání pomocí GET</a><br>";
+// Nastavení cookie s hodnotou
+setcookie('randomNumber', $randomNumber, time() + 3600, "/");
 
+echo "<h1>Přenos hodnoty mezi a.php a b.php různými způsoby</h1>";
+echo "<p>Generovaná hodnota je: <strong>$randomNumber</strong></p>";
+
+// 1. Přenos pomocí GET metody
+echo "<h2>1. Přenos pomocí GET</h2>";
+echo "<a href='b.php?method=get&number=$randomNumber'>Otevřít b.php s GET metodou</a><br>";
+
+// 2. Přenos pomocí POST metody (formulář)
+echo "<h2>2. Přenos pomocí POST</h2>";
 echo "
-    <form action='b.php' method='post'>
-        <input type='hidden' name='value' value='$randomValue'>
-        <button type='submit'>Předání pomocí POST</button>
-    </form>
+<form action='b.php' method='POST'>
+    <input type='hidden' name='method' value='post'>
+    <input type='hidden' name='number' value='$randomNumber'>
+    <button type='submit'>Odeslat číslo pomocí POST</button>
+</form>
 ";
 
-session_start();
-$_SESSION['value'] = $randomValue;
-echo "<a href='b.php?method=session'>Předání pomocí SESSION</a><br>";
+// 3. Přenos pomocí Session
+echo "<h2>3. Přenos pomocí Session</h2>";
+echo "<a href='b.php?method=session'>Otevřít b.php s použitím Session</a><br>";
 
-setcookie('value', $randomValue, time() + 3600, '/');
-echo "<a href='b.php?method=cookie'>Předání pomocí COOKIE</a><br>";
+// 4. Přenos pomocí Cookies
+echo "<h2>4. Přenos pomocí Cookies</h2>";
+echo "<a href='b.php?method=cookie'>Otevřít b.php s použitím Cookies</a><br>";
+
+// 5. Přenos pomocí přesměrování (HTTP 302)
+echo "<h2>5. Přenos pomocí HTTP přesměrování</h2>";
+echo "<form action='b.php' method='GET'>
+    <input type='hidden' name='number' value='$randomNumber'>
+    <input type='hidden' name='method' value='redirect'>
+    <button type='submit'>Přesměrovat na b.php s číslem</button>
+</form>";
 ?>
