f00847570563060299b223596612ed316e3d5501
Rice guy <89035491+cooker-of-rice@users.noreply.github.com>
2026-03-03 09:41:35 +0100

    Update team.php
    fixed loop

--- maturita/zkusebni/team.php
+++ maturita/zkusebni/team.php
@@ -27,9 +27,10 @@ $mvp= $team[0];
 foreach ($team as $player){
     if ($player->goalCount>$mvp->goalCount){
         $mvp = $player;
-        echo $name . "má nejvíce goalů". "(".$player->goalCount. ")<br>";
     }
 }
+
+echo $name . "má nejvíce goalů". "(".$player->goalCount. ")<br>";
 
 $json = json_encode($team,JSON_PRETTY_PRINT);
 file_put_contents("team.json", $json); 
